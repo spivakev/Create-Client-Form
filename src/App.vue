8e0680bc52bf30bab3a8a8c1d6541a33feac27b7
Elena Spivak <spivak.e.v@gmail.com>
2020-11-08 20:06:01 +0300

    clear app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CreateClientForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CreateClientForm from "@/components/CreateClientForm";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CreateClientForm
   }
-}
+};
 </script>
 
 <style>
