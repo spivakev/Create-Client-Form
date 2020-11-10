a0a619588eee667c7ea116307bc99e030375f5e1
Elena Spivak <spivak.e.v@gmail.com>
2020-11-10 11:31:09 +0300

    add validation to the main fields

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App),
