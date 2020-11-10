81ce8273c74c63f17e3cea93e70672a0afd151b2
Elena Spivak <spivak.e.v@gmail.com>
2020-11-10 21:15:37 +0300

    add common input styles

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/*
+input, select {
+  border: 1px solid#a5a5a5;
+  border-radius: 3px;
+}
+*/
+
+input[type="text"],
+input[type="tel"],
+input[type="date"],
+input[type="number"],
+select {
+  padding: 5px 10px;
+}
 </style>
