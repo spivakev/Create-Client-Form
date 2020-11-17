7c55b562265eacfb626547de1b73b7c25e9e1dd7
Elena Spivak <spivak.e.v@gmail.com>
2020-11-17 20:15:09 +0300

    change configs

--- src/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Create-Client-Form/'
-    : '/'
-}
-
-//"https://spivakev.github.io/Create-Client-Form/js/app.4389f8b6.js"
